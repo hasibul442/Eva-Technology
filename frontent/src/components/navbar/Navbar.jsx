bfad8ff6413be89911831d5792499e507b8352a7
Mohammad Hasibul Hasan <hasibulhasan442@gmail.com>
2022-02-03 16:37:18 +0600

    Bootstrap Nav Bar

--- frontent/src/components/navbar/Navbar.jsx
+++ frontent/src/components/navbar/Navbar.jsx
@@ -4,7 +4,9 @@ import './navbar.css';
 
 function Navbar() {
 
-
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
   return (
     <>
@@ -12,9 +14,12 @@ function Navbar() {
         <nav className="navbar navbar-expand-lg  bg-light">
             <div className="container-fluid">
               <Link to='/' className="navbar-brand" href="#"><img src="Eva-Tech-Full-Logo1.png" className='navbar-company-logo'/></Link>
-              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <div className='menu-icon navbar-toggler' onClick={handleClick} data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <i className= {click ? 'fas fa-times' : 'fas fa-bars'}></i>
+              </div>
+              {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
-              </button>
+              </button> */}
               <div className="collapse navbar-collapse " id="navbarSupportedContent" >
                 <ul className="navbar-nav me-auto mx-auto mb-2 mb-lg-0">
                   <li className="nav-item">
