201e1f38ba51416e1f7edd0154bc030144b59265
Mohammad Hasibul Hasan <hasibulhasan442@gmail.com>
2022-02-03 16:30:21 +0600

    Bootstrap Nav Bar

--- frontent/src/components/navbar/Navbar.jsx
+++ frontent/src/components/navbar/Navbar.jsx
@@ -1,57 +1,52 @@
-import React, { useState } from 'react';
-import './navbar.css';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
-// import { click } from '@testing-library/user-event/dist/click';
+import './navbar.css';
 
 function Navbar() {
-  const [click, setClick] = useState(false);
-  const handleClick = () => setClick(!click);
-  const closeMobileMenu = () => setClick(false);
+
+
+
   return (
     <>
-        <nav className='navbar'>
-            <div className='navbar-container'>
-                <Link to="/" className="navbar-logo">
-                    <img src="Eva-Tech-Full-Logo1.png" className='navbar-company-logo'/>
-                </Link>
-                <div className='menu-icon' onClick={handleClick}>
-                  <i className= {click ? 'fas fa-times' : 'fas fa-bars'}></i>
-                </div>
-
-                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-
-                  <li className='nav-item'>
-                    <Link to='/' className='nav-links' onClick={closeMobileMenu}> Home </Link>
+       <div>
+        <nav className="navbar navbar-expand-lg  bg-light">
+            <div className="container-fluid">
+              <Link to='/' className="navbar-brand" href="#"><img src="Eva-Tech-Full-Logo1.png" className='navbar-company-logo'/></Link>
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse " id="navbarSupportedContent" >
+                <ul className="navbar-nav me-auto mx-auto mb-2 mb-lg-0">
+                  <li className="nav-item">
+                    <Link className="nav-link" to='/'>Home</Link>
                   </li>
-{/* 
-                  <li className='nav-item'>
-                    <Link to='/aboutus' className='nav-links' onClick={closeMobileMenu}> About Us </Link>
-                  </li> */}
-
+                  <li className="nav-item">
+                    <Link className="nav-link" to='/projects'>About Us</Link>
+                  </li>
                   <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" herf='#'  id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      About Us
-                    </a>
-                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                      <Link to='/aboutus' className="dropdown-item nav-links" onClick={closeMobileMenu} >About Us</Link>
-                      <Link to='/mission' className="dropdown-item nav-links" onClick={closeMobileMenu} >Mission & Vision</Link>
-                      <Link to='/director' className="dropdown-item nav-links" onClick={closeMobileMenu} >Directors</Link>
-                      <Link to='/patner' className="dropdown-item nav-links" onClick={closeMobileMenu} >Patner</Link>
-                      <Link to='/client' className="dropdown-item nav-links" onClick={closeMobileMenu} >Clients</Link>
-                    </div>
+                    <Link className="nav-link dropdown-toggle" to='/' id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Dropdown
+                    </Link>
+                    <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <li><Link className="dropdown-item" to='/aboutus'>About Us</Link></li>
+                      <li><Link className="dropdown-item" to='/mission'>Mission & Vision</Link></li>
+                      <li><Link className="dropdown-item" to='/director'>Directors</Link></li>
+                      <li><Link className="dropdown-item" to='/patner'>Patners</Link></li>
+                      <li><Link className="dropdown-item" to='/client'>Clients</Link></li>
+                      <li><Link className="dropdown-item" to='/teammember'>Team Member</Link></li>
+                    </ul>
                   </li>
-
-                  <li className='nav-item'>
-                    <Link to='/services' className='nav-links' onClick={closeMobileMenu}> Services </Link>
+                  <li className="nav-item">
+                    <Link className="nav-link" to='/services'>Services</Link>
                   </li>
-                  
-                  <li className='nav-item'>
-                    <Link to='/contact' className='nav-links' onClick={closeMobileMenu}> Contact </Link>
+                  <li className="nav-item">
+                    <Link className="nav-link" to='/contact' >Contact</Link>
                   </li>
                 </ul>
-                
+              </div>
             </div>
-        </nav>
+          </nav>
+       </div>
     </>
   );
 }
