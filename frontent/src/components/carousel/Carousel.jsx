4a7eb2421eb0994acd3dd4b597accd7fa3aa6a58
Mohammad Hasibul Hasan <hasibulhasan442@gmail.com>
2022-02-07 16:26:53 +0600

    Carousel

--- frontent/src/components/carousel/Carousel.jsx
+++ frontent/src/components/carousel/Carousel.jsx
@@ -13,16 +13,29 @@ function Carousel1() {
             showStatus = {false}
             showThumbs= {false}
             stopOnHover= {false}
-            interval = { 5000 }
+            interval = { 10000 }
             >
                 <div>
-                    <img src="carousel-image/1.jpg" className='carousel-image'/>
-                    {/* <p className="legend">Legend 1</p> */}
+                    <div className="effect grid-width8 effect-six">
+                    <img src="carousel-image/1.jpg" className='carousel-image' alt="Image One"/>
+                    <div className="tab-text">
+                        <h2>grid column 8</h2>
+                        <p>Era of modern design </p>
+                        {/* <div className="icons-block"> <a href="#" className="social-icon-1"><i className="fa fa-facebook-official"></i></a> <a href="#" className="social-icon-2"><i className="fa fa-twitter-square"></i></a> <a href="#" className="social-icon-3"><i className="fa fa-youtube-square"></i></a> </div> */}
+                    </div>
+                    </div>
                 </div>
                 <div>
+                    <div className="effect grid-width8 effect-six">
                     <img src="carousel-image/2.jpg" className='carousel-image'/>
-                    {/* <p className="legend">Legend 2</p> */}
+                    <div className="tab-text">
+                        <h2>grid column 8</h2>
+                        <p>Era of modern design </p>
+                        {/* <div className="icons-block"> <a href="#" className="social-icon-1"><i className="fa fa-facebook-official"></i></a> <a href="#" className="social-icon-2"><i className="fa fa-twitter-square"></i></a> <a href="#" className="social-icon-3"><i className="fa fa-youtube-square"></i></a> </div> */}
+                    </div>
+                    </div>
                 </div>
+               
                 <div>
                     <img src="carousel-image/3.jpg" className='carousel-image'/>
                     {/* <p className="legend">Legend 3</p> */}
